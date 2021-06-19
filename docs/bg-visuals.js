3152a3feb5d7a6ec510e9c59102b65a6872fdf21
Austin Farmer <austinfarmer954@gmail.com>
2021-06-19 10:06:59 -0400

    Removed canvas test script

--- docs/bg-visuals.js
+++
@@ -1,9 +0,0 @@
-console.log("loading background");
-
-
-var canvas = document.getElementById("background");
-var context = canvas.getContext("2d");
-
-context.beginPath();
-context.arc(95, 50, 40, 0, 2 * Math.PI);
-context.stroke();
